2648f786c6191a5441e480c6f63b855be4bb70ef
ypmk <kopylovaksusha123@gmail.com>
2019-10-16 17:06:19 +0300

    Android Init Commit

---
+++ app/src/main/java/com/example/sumsung_homework/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sumsung_homework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
