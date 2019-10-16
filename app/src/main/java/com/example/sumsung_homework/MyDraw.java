2a8c36741b64d50215ad4faf00479c3ad714c659
ypmk <kopylovaksusha123@gmail.com>
2019-10-16 17:42:56 +0300

    1.9.5 Выполненное дз

--- app/src/main/java/com/example/sumsung_homework/MyDraw.java
+++ app/src/main/java/com/example/sumsung_homework/MyDraw.java
@@ -1,4 +1,46 @@
 package com.example.sumsung_homework;
 
-public class MyDraw {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyDraw extends View {
+
+    int N = 50; // количество шариков
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
+    public MyDraw(Context context) {
+        super(context);
+        for (int i = 0; i < N; i++) {
+            x[i] = (float) (Math.random() * 500);
+            y[i] = (float) (Math.random() * 500);
+            vx[i] = (float) (Math.random() * 6 - 3);
+            vy[i] = (float) (Math.random() * 6 - 3);
+        }
+    }
+
+    Paint paint = new Paint();
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // отрисовываем все шарики
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 20, paint);
+        }
+        // готовим массивы x и у для следущего кадра
+        for (int i = 0; i < N; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
+        }
+        // Запрос на перерисовку экрана
+        invalidate();
+    }
 }
